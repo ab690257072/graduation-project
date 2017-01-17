ccc25c2d98f6927a425ce1f5bedc942d9c92324a
ab690257072 <690257072@qq.com>
2017-01-17 18:21:05 +0800

    add project

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+	<h2>hello world!</h2>
+	<h3>{{msg}}</h3>
+	<button class="btn" @click="change">try it!</button>
+	<my-menu></my-menu>
+</template>
+<script>
+	import Menu from './components/Menu.vue';
+
+	export default {
+		data() {
+			return {
+				msg: 'zhao'
+			};
+		},
+		methods: {
+			change() {
+				this.msg = 'msg is changed';
+			}
+		},
+		components: {
+			'my-menu': Menu
+		}
+	};
+</script>
+<style>
+	.btn {
+		padding: .3em .8em;
+		border: 1px solid deepskyblue;
+		background: #fff;
+		border-radius: .2em;
+		color: deepskyblue;
+		font-size: 125%;
+		line-height: 1.5;
+		outline: none;
+	}
+</style>
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # graduation-project
-This is my graduation-project in jirengu
+## intro
+This is my graduation-project in jirengu.
+## usage
+* 所有的依赖已写入package.json中，可以用`npm install`或`cnpm install`如下命令快速安装依赖：
+```
+npm install
+```
+```
+cd graduation-project
+```
+* 如果您的命令环境中有webpack-dev-server，可以用如下命令快速运行项目：
+```
+npm run dev
+```
